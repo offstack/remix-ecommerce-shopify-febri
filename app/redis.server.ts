1e282353f49c3d287c0e51bc44ba64f4a9acfaa3
febriansyah11 <1102.adiputra.com>
2022-11-15 00:41:18 +0700

    Remove redis file

--- app/redis.server.ts
+++ app/redis.server.ts
@@ -1,18 +1,18 @@
-import { createClient } from "redis";
-console.log('process.env.REDIS_URL => ',process.env.REDIS_URL)
-if (!process.env.REDIS_URL) {
-  throw new Error("REDIS_URL environment variable is not set");
-}
+// import { createClient } from "redis";
+// console.log('process.env.REDIS_URL => ',process.env.REDIS_URL)
+// if (!process.env.REDIS_URL) {
+//   throw new Error("REDIS_URL environment variable is not set");
+// }
 
-declare global {
-  var redisClient: ReturnType<typeof createClient>;
-}
+// declare global {
+//   var redisClient: ReturnType<typeof createClient>;
+// }
 
-let redisClient = (global.redisClient =
-  global.redisClient ||
-  createClient({
-    url: process.env.REDIS_URL,
-  }));
+// let redisClient = (global.redisClient =
+//   global.redisClient ||
+//   createClient({
+//     url: process.env.REDIS_URL,
+//   }));
 
-export default redisClient;
+// export default redisClient;
 
