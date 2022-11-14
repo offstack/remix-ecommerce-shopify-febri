1c0737c23cb9caffa79e2dab6f9357066a5970da
febriansyah11 <1102.adiputra.com>
2022-11-15 00:55:54 +0700

    Update redis file"

--- app/redis.server.ts
+++ app/redis.server.ts
@@ -1,5 +1,5 @@
 // import { createClient } from "redis";
-// console.log('process.env.REDIS_URL => ',process.env.REDIS_URL)
+
 // if (!process.env.REDIS_URL) {
 //   throw new Error("REDIS_URL environment variable is not set");
 // }
@@ -8,11 +8,11 @@
 //   var redisClient: ReturnType<typeof createClient>;
 // }
 
-// let redisClient = (global.redisClient =
-//   global.redisClient ||
-//   createClient({
-//     url: process.env.REDIS_URL,
-//   }));
+let redisClient = (global.redisClient =
+  global.redisClient ||
+  createClient({
+    url: process.env.REDIS_URL,
+  }));
 
-// export default redisClient;
+export default redisClient;
 
