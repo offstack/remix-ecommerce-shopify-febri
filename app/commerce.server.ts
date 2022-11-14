0b08d922e0ac9d6ec8fb20727e3aaecf1b4142c7
febriansyah11 <1102.adiputra.com>
2022-11-14 22:53:01 +0700

    Remove redis

--- app/commerce.server.ts
+++ app/commerce.server.ts
@@ -13,9 +13,9 @@ let commerce = createShopifyProvider({
   shop: process.env.SHOPIFY_STORE!,
   storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
   maxAgeSeconds: 60,
-  cache: createSwrRedisCache({
-    redisClient,
-  }),
+  // cache: createSwrRedisCache({
+  //   redisClient,
+  // }),
 });
 
 export default commerce;
