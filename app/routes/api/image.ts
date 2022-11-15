2e150bcc577efe77cdfbb24aaa48ea83e8041bf3
febriansyah11 <1102.adiputra.com>
2022-11-15 17:31:16 +0700

    Custom image loader

--- app/routes/api/image.ts
+++ app/routes/api/image.ts
@@ -1,20 +1,31 @@
 import { type LoaderArgs } from "@remix-run/node";
-import { imageLoader, DiskCache, fetchResolver } from "remix-image/server";
 import { sharpTransformer } from "remix-image-sharp";
+import type { LoaderFunction } from "@remix-run/server-runtime";
+import { imageLoader, DiskCache } from "remix-image/server";
 export function loader({ request }: LoaderArgs) {
   let url = new URL("/", request.url);
 
-  return imageLoader(
-    {
-      selfUrl: url.href,
-      cache: new DiskCache({
-        path: "../../../.cache/images",
-      }),
-      resolver: async (asset, url, options, basePath) => {
-        return await fetchResolver(asset, url, options, basePath);
-      },
-      transformer: sharpTransformer,
-    },
-    request
-  );
+  const config = {
+    selfUrl: url.href,
+    cache: new DiskCache(),
+  };
+  
+  return imageLoader(config, request);
+
+  // return imageLoader(
+  //   {
+  //     selfUrl: url.href,
+  //     cache: new DiskCache({
+  //       path: "../../../.cache/images",
+  //     }),
+  //     resolver: async (asset, url, options, basePath) => {
+  //       return await fetchResolver(asset, url, options, basePath);
+  //     },
+  //     transformer: sharpTransformer,
+  //   },
+  //   request
+  // );
+
+
+  
 }
