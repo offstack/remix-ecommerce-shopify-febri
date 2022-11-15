d64ab1ff4ab28c53b9b1e47c358fb3e08f7ef938
febriansyah11 <1102.adiputra.com>
2022-11-15 19:36:41 +0700

    Custom image loader, still handle image not show

--- app/routes/api/image.ts
+++ app/routes/api/image.ts
@@ -1,3 +1,26 @@
-import { loader } from "~/../loader/route.server";
+import { type LoaderArgs } from "@remix-run/node";
+import { imageLoader, DiskCache, fetchResolver } from "remix-image/server";
+import { sharpTransformer } from "remix-image-sharp";
 
-export { loader };  
+export function loader({ request }: LoaderArgs) {
+    try {
+        let url = new URL("/", request.url);
+
+        return imageLoader(
+            {
+            selfUrl: url.href,
+            cache: new DiskCache({
+                path: ".cache/images",
+            }),
+            resolver: async (asset, url, options, basePath) => {
+                return await fetchResolver(asset, url, options, basePath);
+            },
+            transformer: sharpTransformer,
+            },
+            request
+        );
+  } catch (error) {
+    console.log("🚀 ~ file: image.ts ~ line 23 ~ loader ~ error", error)
+    return null 
+  }
+}
